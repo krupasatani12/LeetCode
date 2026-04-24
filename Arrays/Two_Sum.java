095a5c91737f8d3c18df08cb8a5bb11970e8b0e3
krupasatani12 <krupasatani12@gmail.com>
2026-04-24 22:50:07 +0530

    Updated Arrays folder

--- Arrays/Two_Sum.java
+++ Arrays/Two_Sum.java
@@ -1,4 +1,4 @@
-class TwoSum{
+class Two_Sum{
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
